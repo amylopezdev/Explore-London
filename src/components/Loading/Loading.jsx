ab61382e555511e36b7c1b8257d414b56af45faa
Amy Lopez <amy.lopez@sainsburysbank.co.uk>
2024-06-19 15:26:45 +0100

    make loading spinner accessible

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 
 export const Loading = () => {
   return (
-    <div className="loading-container">
+    <div className="loading-container" aria-role="status" aria-label="label">
       <div className="lds-ripple">
         <div></div>
         <div></div>
